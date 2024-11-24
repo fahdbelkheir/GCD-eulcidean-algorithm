08d40e6e33391548428451b598b8c23c28c3a1d6
fahdbelkheir <fahed.belkheiri@gmail.com>
2024-11-24 23:43:48 +0100

    gcg02

--- gcd.py
+++ gcd.py
@@ -3,24 +3,21 @@ def compute_gcd(a, b):
         a, b = b, a % b
     return a
 
-
-# Main function
 def main():
     try:
-        # Prompt the user to enter two integers
+        
         A = int(input("Enter the first integer (A): "))
         B = int(input("Enter the second integer (B): "))
 
-        # Compute the GCD of the two numbers
+        
         gcd = compute_gcd(A, B)
 
-        # Display the result
+       
         print(f"The GCD of {A} and {B} is: {gcd}")
 
     except ValueError:
         print("Please enter valid integers.")
 
 
-# Correctly call the main function
 if __name__ == "__main__":
     main()
